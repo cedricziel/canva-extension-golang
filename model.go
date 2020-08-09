5bfa744c053e3bab661bc675eac65215377f98b0
Cedric Ziel <cedric@cedric-ziel.com>
2020-08-09 18:43:21 +0200

    Add helper method to facilitate response

--- model.go
+++ model.go
@@ -1,5 +1,10 @@
 package canva
 
+import (
+	"encoding/json"
+	"net/http"
+)
+
 type CanvaAsset struct {
 	Name string `json:"name"`
 	Type string `json:"type"`
@@ -16,4 +21,19 @@ type CanvaMessage struct {
 
 type CanvaResponse struct {
 	Type string `json:"type"`
+}
+
+func NewSuccessResponse() CanvaResponse {
+	return CanvaResponse{Type: "SUCCESS"}
+}
+
+// WriteSuccessResponse will write a correct success response
+// Canva is really picky about response codes and expects
+// a status of 200 and an inline success.
+func WriteSuccessResponse(w http.ResponseWriter) {
+	json, _ := json.Marshal(NewSuccessResponse())
+
+	w.WriteHeader(http.StatusOK)
+	w.Header().Add("content-type", "application/json")
+	w.Write(json)
 }
